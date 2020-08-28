a0843911544a18d541f3c59f32f7b7942bb30799
Marlon <dracomarmol@gmail.com>
2020-08-27 18:29:57 -0600

    Create clase1.R

---
+++ R/clase1.R
@@ -0,0 +1,30 @@
+# librerias ---------------------------------------------------------------
+# es importante darle control enter al codigo
+library(readxl)
+# bases de datos ----------------------------------------------------------
+imports_resumen_19 <- read_excel("C:/Users/Marlon.molina/Ministerio de Comercio Exterior/Monitoreo - Documentos/Costa Rica/Importaciones/2019/NUEVO_Ener-Dic_ 2019_Resumen.Rev21022020 .xlsx")
+#
+
+cuadrado <- function(par){
+  par2 <- par^2
+}
+
+r1 <- cuadrado(5)
+remove(r1)
+# Funciones 
+
+# Llamamos la funcion que me permite cambiar el nombre de las columnas
+colnames(imports_resumen_19)[2] <- "mes"
+
+
+colna
+
+# Operadores
+5 > 5
+5 >= 6
+7 <= 8
+
+# Clases dos
+# tipos de objetos
+# la importancia de entender el problema y hacerlo reproducible
+# ejemplo practico
