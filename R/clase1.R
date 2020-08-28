8bf6a9a0a07918e9befda490617de359da38ad5e
Marlon <dracomarmol@gmail.com>
2020-08-27 21:51:03 -0600

    Update clase1.R

--- R/clase1.R
+++ R/clase1.R
@@ -2,7 +2,7 @@
 # es importante darle control enter al codigo
 library(readxl)
 # bases de datos ----------------------------------------------------------
-imports_resumen_19 <- read_excel("C:/Users/Marlon.molina/Ministerio de Comercio Exterior/Monitoreo - Documentos/Costa Rica/Importaciones/2019/NUEVO_Ener-Dic_ 2019_Resumen.Rev21022020 .xlsx")
+#imports_resumen_19 <- read_excel("C:/Users/Marlon.molina/Ministerio de Comercio Exterior/Monitoreo - Documentos/Costa Rica/Importaciones/2019/NUEVO_Ener-Dic_ 2019_Resumen.Rev21022020 .xlsx")
 #
 
 cuadrado <- function(par){
