3359c60997a9b4214b064146e5657150c06db977
lor3nte <73676139+lor3nte@users.noreply.github.com>
2020-11-05 17:11:56 -0600

    Update clase5_tarea.R

--- R/clase5_tarea.R
+++ R/clase5_tarea.R
@@ -8,8 +8,18 @@ pib_desagregado <- read_xlsx("data/pib_desagregado.xlsx", skip = 4) %>%
 # Se elimina la primera fila y se escogen solo las que se necesitan
   slice(-1) %>% 
   slice(5,8,10,13) %>% 
+  rename("variable" = "...1") 
+  
+#transponer la tabla
+  pib_transpuesto <- data.frame(t(pib_desagregado[-1])) %>% 
+  rename("G"="X1") %>% 
+  rename("C"="X2") %>% 
+  rename("X"="X3") %>% 
+  rename("M"="X4") %>% 
+    
+    #balanza comercial
+    mutate(balanza=X-M) %>% 
+    mutate(pib=G+C+balanza)
 
-  rename("variable" = "...1")
 
-
-
+  
