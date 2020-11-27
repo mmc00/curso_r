30604eebd2e6fcf944987acbffaf371587ede08a
marioalr20 <72947955+marioalr20@users.noreply.github.com>
2020-11-26 18:28:46 -0600

    Update clase 6 mip.R

--- R/clase 6 mip.R
+++ R/clase 6 mip.R
@@ -1,9 +1,8 @@
 library(tidyverse)
 library(readxl)
 
-MIP_77 <-read_xlsx("C:\Users\Sebastian\Documents\GitHub\curso_rcdata\MIP77.xlsx")
 
-# Se carga la base y se quitan las primeras 7 filas
-MIP_77 <- read_xlsx("MIP77.xlsx", skip = 7) 
+# Se carga la base y se quitan las primeras 7 fil as
+MIP_77 <- read_xlsx("data/MIP77.xlsx", skip = 7)
   
   
